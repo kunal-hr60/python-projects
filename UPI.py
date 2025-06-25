0d323a62ec4ad267d3f757bf58bb44ae1f17b11c
kunal-hr60 <kunaljangra2532@gnail.com>
2025-06-25 11:44:13 +0530

    update variables name and add comments

--- UPI.py
+++ UPI.py
@@ -1,18 +1,18 @@
-# a,b,c,d,p,q,x,e,y
+# pin_choice,balance_choice,pay_amount,ballance,default_pin,q,x,input_pin,y
 def pay():
     def transection():
-        d=ammount
-        No=int(input("pay to Number : "))
-        c=int(input("Enter you ammount to pay : "))
-        if c<=d:
+        ballance=ammount
+        mobile_no=int(input("pay to Number : "))
+        pay_amount=int(input("Enter you ammount to pay : "))
+        if pay_amount<=ballance:
             x=0
             while (x!=3):          #loop for Giving Chance For Correct pin
                 x=x+1
                 pin=int(input("Enter your pin "))
-                if pin==p: 
+                if pin==default_pin: 
                     print("Processing Transection")
-                    print("your payment of ",c," is succesfull to ",No)
-                    print("Your current balance is : ",d-c)
+                    print("your payment of ",pay_amount," is succesfull to ",mobile_no)
+                    print("Your current balance is : ",ballance-pay_amount)
                     break    # Function Ends
                 else:
                     print("WaRNING : Wrong Pin\n        Payment failed")
@@ -27,16 +27,16 @@ def pay():
     print("optin :\n")
     print("1. Add Money")
     print("2. Continue\n")
-    b=int(input(print("chose One\n")))
-    if b==1:
-        c=int(input("Enter the ammount you want to add "))
-        y=0
+    balance_choice=int(input(print("chose One\n")))
+    if balance_choice==1:
+        pay_amount=int(input("Enter the ammount you want to add "))
+        y=0             #loop for ammount update
         while (y!=3):
             y=y+1
-            e=int(input(print("Enter your pin")))
-            print("\n Updated Balance : ",ammount+c)
-            if e==p:
-                ammount+=c
+            input_pin=int(input(print("Enter your pin")))
+            print("\n Updated Balance : ",ammount+pay_amount)
+            if input_pin==default_pin:
+                ammount+=pay_amount
                 transection()
                 break
             else:
@@ -47,17 +47,17 @@ def pay():
         transection()
     print("THaNK YOU FOR SHOWING YOUR INTREST ")
 # Main function start from here
-input(print("Enter your Card Number"))        #use if update Further
+input(print("Enter your Card Number"))        #user data required in Further update
 input(print("Enter your card cvv"))
 print("Default pin = 0\n")
 print("option\n")
 print("1.Change Pin")
 print("2. Continue\n")
-a=int(input(print("Chose One")))
-if a==1:
+pin_choice=int(input(print("Chose One")))
+if pin_choice==1:
     Tpin=int(input(print("\nEnter New pin")))
     Mpin=int(input(print("Confirm Pin")))
-    p=Tpin
+    default_pin=Tpin
     if Tpin==Mpin:
         print("\nYour New pin set up is Succesfull")
         print("New pin : ",Tpin)
@@ -65,8 +65,8 @@ if a==1:
         pay()
     else:
         print("pin doesn't match")
-elif a==2:
-    p=0
+elif pin_choice==2:
+    default_pin=0
     pay()
 else:
     print("Invalid Choice")
